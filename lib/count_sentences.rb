c9d4c1c157f60c8c7ee357b2f4fc7d4fbce7c345
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-11-02 10:07:03 -0600

    commit tree 3d17baf4057ed26cf554856536eea1fb541ce57b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
-
+  total = 0
   def count_sentences
-
+    self.split.filter { |frag| frag.end_with?(".", "?", "!")}.length
+    # split_self.length
   end
 end
